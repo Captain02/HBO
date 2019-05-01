f6a512960182aa19fa030462bb484cae17857086
1076453997@qq.com <090800liukaize>
2019-05-01 20:14:02 +0800

    跨域设置
    跨域设置

--- api/baseapi/renren-admin/src/main/java/io/renren/common/config/WebConfig.java
+++ api/baseapi/renren-admin/src/main/java/io/renren/common/config/WebConfig.java
@@ -8,7 +8,11 @@
 
 package io.renren.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,4 +29,18 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
     }
 
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
 }
