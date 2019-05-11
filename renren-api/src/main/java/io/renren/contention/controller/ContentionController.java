b5be58bc7a4f976834ba743b0ae92f1d4f93250f
dadaokongkong <1961484949@qq.com>
2019-05-11 23:26:10 +0800

    争鸣板块
    争鸣板块

---
+++ renren-api/src/main/java/io/renren/contention/controller/ContentionController.java
@@ -0,0 +1,12 @@
+package io.renren.contention.controller;
+
+import io.renren.common.controller.BaseController;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contention")
+@Api("争鸣板块")
+public class ContentionController  extends BaseController {
+}
---
+++ renren-api/src/main/java/io/renren/contention/service/ContentionService.java
@@ -0,0 +1,7 @@
+package io.renren.contention.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentionService {
+}
