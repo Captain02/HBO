c10365d933aadbbec217a8b80caa4b3fb0c33e6f
1076453997@qq.com <090800liukaize>
2019-05-12 10:09:20 +0800

    修改工具类
    修改工具类

--- renren-common/src/main/java/io/renren/common/utils/checkParameterUtil.java
+++ renren-common/src/main/java/io/renren/common/utils/checkParameterUtil.java
@@ -2,7 +2,7 @@ package io.renren.common.utils;
 
 import java.util.Map;
 
-public class CheckParameterUtil {
+class CheckParameterUtil {
 
     public static void checkParameterMap(Map map, String... parameters) {
         if (parameters.length != 0) {
