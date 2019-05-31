f42f6fd14dba6d560075f5effe66a0f6abe86af2
1076453997@qq.com <090800liukaize>
2019-05-31 23:57:25 +0800

    微信修改
    微信修改

--- renren-admin/src/main/java/io/renren/modules/wechart/WechartController.java
+++ renren-admin/src/main/java/io/renren/modules/wechart/WechartController.java
@@ -9,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
 
-@RestController
+@Controller
 public class WechartController {
     private static final Logger logger = LoggerFactory.getLogger(JWTFilter.class);
     @Autowired
@@ -71,6 +73,7 @@ public class WechartController {
         return pcRedirectUrl.toString();
     }
 
+    @ResponseBody
     @GetMapping("/wechart/OAuth/MP_verify_BFl3ph7g1kvJ0PUb.txt")
     public String config(HttpServletResponse response, HttpServletRequest request) throws IOException {
         System.out.println("config............");
@@ -86,6 +89,7 @@ public class WechartController {
         return "BFl3ph7g1kvJ0PUb";
     }
 
+    @ResponseBody
     @GetMapping("/wechart")
     public String getOpenid(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
