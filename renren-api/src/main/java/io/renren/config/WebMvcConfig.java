99f12cbe53d2fcfc5b4540aff6a60b80230f21e1
1076453997@qq.com <090800liukaize>
2019-06-04 23:52:49 +0800

    跨域测试
    跨域测试

--- renren-api/src/main/java/io/renren/config/WebMvcConfig.java
+++ renren-api/src/main/java/io/renren/config/WebMvcConfig.java
@@ -37,20 +37,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(authorizationInterceptor).addPathPatterns("/**");
     }
 
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
-        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
-        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
-        return corsConfiguration;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
-        return new CorsFilter(source);
-    }
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+//        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+//        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig()); // 4
+//        return new CorsFilter(source);
+//    }
 
 //    @Override
 //    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
