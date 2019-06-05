b24b9bcbe932ef6578819241a91b54756fc1c083
1076453997@qq.com <090800liukaize>
2019-06-05 14:48:52 +0800

    前台登录
    前台登录

--- renren-api/src/main/java/io/renren/modules/login/controller/ApiLoginController.java
+++ renren-api/src/main/java/io/renren/modules/login/controller/ApiLoginController.java
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
@@ -15,18 +15,19 @@ import io.renren.common.entity.PageData;
 import io.renren.common.utils.JWTUtil;
 import io.renren.common.utils.R;
 import io.renren.common.utils.RedisUtils;
-import io.renren.common.validator.ValidatorUtils;
-import io.renren.form.LoginForm;
 import io.renren.modules.login.service.TokenService;
 import io.renren.modules.login.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -36,7 +37,7 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("/api")
-@Api(tags="登录接口")
+@Api(tags = "登录接口")
 public class ApiLoginController extends BaseController {
     @Autowired
     private UserService userService;
@@ -45,13 +46,17 @@ public class ApiLoginController extends BaseController {
     @Autowired
     RedisUtils redisUtils;
 
+    @ApiOperation(value = "登录", tags = {"登录接口"})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", paramType = "query", value = "学号", required = true, dataType = "string"),
+            @ApiImplicitParam(name = "password", paramType = "query", value = "密码", required = true, dataType = "string"),
+    })
     @PostMapping("/sys/login")
-    @ApiOperation("登录")
     public R login() throws Exception {
         PageData pageData = this.getPageData();
         String token = userService.userLogin(pageData);
-        if (token == null){
-            return R.error(401,"登录失败");
+        if (token == null) {
+            return R.error(401, "登录失败");
         }
         //表单校验
 //        ValidatorUtils.validateEntity(form);
@@ -59,28 +64,32 @@ public class ApiLoginController extends BaseController {
 //        //用户登录
 //        Map<String, Object> map = userService.login(form);
 
-        return R.ok().put("token",token);
+        return R.ok().put("token", token);
     }
 
+    @ApiOperation(value = "刷新token,请求头带上Authorization即可", tags = {"登录接口"})
     @Login
     @GetMapping("/sys/refreshToken")
-    public R refresh(HttpServletRequest req){
+    public R refresh(HttpServletRequest req) {
         String tokenReq = req.getHeader("Authorization");
         String username = JWTUtil.getUsername(tokenReq);
-        String uuidPassword = UUID.randomUUID().toString().replace("-","");
+        String uuidPassword = UUID.randomUUID().toString().replace("-", "");
         String newToken = JWTUtil.sign(username, uuidPassword);
-        redisUtils.set(username,newToken,60 * 60 * 1);
-        return R.ok().put("token",newToken);
+        redisUtils.set(username, newToken, 60 * 60 * 1);
+        return R.ok().put("token", newToken);
     }
 
+    @ApiOperation(value = "退出", tags = {"登录接口"})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", paramType = "query", value = "学号", required = true, dataType = "string")
+    })
     @Login
     @PostMapping("/logout")
-    @ApiOperation("退出")
-    public R logout(@ApiIgnore @RequestAttribute("userId") long userId){
-        tokenService.expireToken(userId);
+    public R logout() {
+        PageData pageData = this.getPageData();
+        redisUtils.set(pageData.getValueOfString("username"), "", 0);
         return R.ok();
     }
-
 
 
 }
