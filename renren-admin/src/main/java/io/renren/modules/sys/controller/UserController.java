fe3eeb1a1f4a0b6465a66ac8300fda467127b632
1076453997@qq.com <090800liukaize>
2019-07-28 12:15:12 +0800

    修改前后台bug
    修改前后台bug

--- renren-admin/src/main/java/io/renren/modules/sys/controller/UserController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(tags = "用户")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/sys/user")
 public class UserController extends BaseController {
     @Autowired
     private SysUserService sysUserService;
